cbf98a28a2d747137747b29634104ab2ef650a1b
rakibul <rakibuldevs@gmail.com>
2025-02-14 12:30:02 +0600

    add hero section to the website

--- src/components/header/Hero.jsx
+++ src/components/header/Hero.jsx
@@ -1,4 +1,41 @@
+import { heroItems } from "@/constans";
+import Image from "next/image";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="md:pt-[112px] pb-[64px] pt-[96px]">
+      <div className="flex lg:justify-between lg:gap-x-14">
+        <div className="lg:max-w-[810px]">
+          <h1 className="text-primary font-bold lg:text-[72px] lg:leading-[80px] lg:tracking-[-1.8px] lg:mx-auto text-[30px] leading-[36px] mx-auto">
+            Learn from your favorite creators about{" "}
+            <span className="text-[#ffb74cd0]">fantasy reports</span>
+          </h1>
+          <div className="flex md:items-center md:flex-row flex-col md:gap-x-[6px] md:pt-[19px] pt-[64px]">
+            <ul className="flex  gap-x-0 md:gap-x-[-5px]">
+              {heroItems.map((item) => (
+                <li>
+                  <Image src={item.image} alt="" />
+                </li>
+              ))}
+            </ul>
+            <div className="md:pt-0 pt-4">
+              <p className="md:text-[20px] font-medium md:leading-[36px] md:tracking-[-0.6px]  text-secondary text-base tracking-[-0.4px]">
+                5000+{" "}
+                <span className="text-primary">satisfied clients in 2025</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="hidden border border-solid border-primary lg:flex items-center justify-center rounded-full w-52 h-52">
+          <div className="bg-primary rounded-[164px] flex text-white justify-center items-center flex-col px-8 py-6">
+            <p className="text-[48px] font-bold">1:1</p>
+            <button className="text-md font-bold tracking-[-0.5px]">
+              video calls
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default Hero;
--- src/constans/index.js
+++ src/constans/index.js
@@ -1,5 +1,17 @@
+import icons1 from "@/public/hero/icons1.png";
+import icons2 from "@/public/hero/icons2.png";
+import icons3 from "@/public/hero/icons3.png";
+import icons4 from "@/public/hero/icons4.png";
+import icons5 from "@/public/hero/icons5.png";
 export const navItems = [
   { title: "Explore", id: 1, path: "/explore" },
   { title: "Company", id: 2, path: "/company" },
   { title: "Support", id: 3, path: "/support" },
+];
+export const heroItems = [
+  { id: 4, image: icons1 },
+  { id: 5, image: icons2 },
+  { id: 6, image: icons3 },
+  { id: 7, image: icons4 },
+  { id: 8, image: icons5 },
 ];
