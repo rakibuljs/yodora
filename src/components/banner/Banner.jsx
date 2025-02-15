d9dd379f24d4d9c6b3e1c3bc7cf1b7942df64bf6
rakibul <rakibuldevs@gmail.com>
2025-02-15 09:13:30 +0600

    add banner section to the website

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,4 +1,13 @@
+import BannerImage from "@/public/Dashboard.png";
+import Image from "next/image";
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <section className="relative">
+      <div className="absolute bottom-0 bg-primary w-full h-1/2 -z-10" />
+      <div className="lg:container grid justify-center items-center px-4 ">
+        <Image src={BannerImage} alt="bannerimage" className="object-cover" />
+      </div>
+    </section>
+  );
 };
 export default Banner;
