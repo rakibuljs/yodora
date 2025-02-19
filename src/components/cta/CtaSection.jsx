2fdc2d982e95aa7c97533d04de977802ae60a15d
rakibul <rakibuldevs@gmail.com>
2025-02-19 15:10:48 +0600

    add title to website

--- src/components/cta/CtaSection.jsx
+++ src/components/cta/CtaSection.jsx
@@ -2,7 +2,7 @@ const CtaSection = () => {
   return (
     <section className="md:container pt-[64px] lg:px-0 px-4">
       <div className="relative flex flex-col items-center bg-primary px-8 py-28 rounded-2xl overflow-hidden">
-        <div className="top-0 z-10 absolute bg-accent-500 w-1/5 h-2" />
+        <div className="top-0 z-10 absolute bg-secondary w-1/5 h-2" />
         <div className="left-[4%] absolute bg-gradient-to-l from-white to-transparent opacity-20 blur-2xl rounded-[50%] w-[50rem] h-28 -rotate-45" />
         <div className="left-[35%] absolute bg-gradient-to-l from-white to-transparent opacity-20 blur-2xl rounded-[50%] w-[40rem] h-28 -rotate-45 visible" />
 
--- src/components/header/Navigation.jsx
+++ src/components/header/Navigation.jsx
@@ -1,50 +1,78 @@
+"use client";
 import { navItems } from "@/constans";
 import Logo from "@/public/Logo.svg";
 import Image from "next/image";
 import Link from "next/link";
 import Menu from "@/public/menu.svg";
+import { IoClose } from "react-icons/io5";
+import { useState } from "react";
 const Navigation = () => {
+  const [isMenu, setIsMenu] = useState(false);
+  const handleMenuSetup = () => {
+    setIsMenu(!isMenu);
+  };
   return (
-    <nav className="flex items-center justify-between md:pt-[32px] pt-4">
-      <div className="flex items-center gap-x-8">
-        <Image
-          src={Logo}
-          alt="logo"
-          className="inline-block w-[72px] h-[24px] md:w-[93.024px]"
-          height={23}
-          width={93}
-        />
-        <ul className="hidden lg:flex items-center gap-x-8">
+    <div>
+      <nav className="flex items-center justify-between md:pt-[32px] pt-4">
+        <div className="flex items-center gap-x-8">
+          <Image
+            src={Logo}
+            alt="logo"
+            className="inline-block w-[72px] h-[24px] md:w-[93.024px]"
+            height={23}
+            width={93}
+          />
+          <ul className="hidden lg:flex items-center gap-x-8">
+            {navItems.map((item) => (
+              <li
+                key={item.id}
+                className="text-primary font-medium text-lg hover:text-secondary transition-colors duration-500"
+              >
+                <Link href={item.path}>{item.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="hidden lg:flex gap-x-8">
+          <button className="text-primary text-lg font-medium hover:text-secondary transition-colors duration-500">
+            Sign in
+          </button>
+          <button className="bg-primary px-8 py-[10px] text-white rounded-2xl hover:bg-secondary transition-colors duration-500">
+            Get Started
+          </button>
+        </div>
+        <div className="lg:hidden block">
+          <Link href="" onClick={handleMenuSetup}>
+            {isMenu ? (
+              <IoClose size={20} />
+            ) : (
+              <Image
+                src={Menu}
+                alt="menu"
+                className="inline-block"
+                width={24}
+                height={24}
+              />
+            )}
+          </Link>
+        </div>
+      </nav>
+      {isMenu && (
+        <ul className="lg:hidden  md:flex flex-col items-center gap-5 right-[20px] absolute mt-4 bg-secondary p-10  rounded-xl pb-10 z-50 shadow-lg">
           {navItems.map((item) => (
             <li
               key={item.id}
-              className="text-primary font-medium text-lg hover:text-secondary transition-colors duration-500"
+              className="text-primary font-medium text-sm hover:text-white transition-colors duration-500"
             >
               <Link href={item.path}>{item.title}</Link>
             </li>
           ))}
+          <button className="font-medium hover:text-white transition-colors duration-500 bg-black text-white px-4 py-1 rounded-xl mt-2 text-sm">
+            Sign in
+          </button>
         </ul>
-      </div>
-      <div className="hidden lg:flex gap-x-8">
-        <button className="text-primary text-lg font-medium hover:text-secondary transition-colors duration-500">
-          Sign in
-        </button>
-        <button className="bg-primary px-8 py-[10px] text-white rounded-2xl hover:bg-secondary transition-colors duration-500">
-          Get Started
-        </button>
-      </div>
-      <div className="lg:hidden block">
-        <a href="">
-          <Image
-            src={Menu}
-            alt="menu"
-            className="inline-block"
-            width={24}
-            height={24}
-          />
-        </a>
-      </div>
-    </nav>
+      )}
+    </div>
   );
 };
 export default Navigation;
